a6c5d47ad8e5a8e9faf71ba4c19b2349eba2c923
Jinny Sung <75344406+sjinicd@users.noreply.github.com>
2021-05-12 17:59:42 +0900

    Create 2021.05.12.js

---
+++ 2021.05.12.js
@@ -0,0 +1,9 @@
+![](https://images.velog.io/images/jinheesung/post/3c6e7844-7fcb-4a57-9fa2-5d118104cda8/image.png)
+
+
+return 값이 필요할 경우 -> 데이터 타입을 int로 설정한다.(update에서 사용)
+return 즉, 받아오는 것이 확실하다고 생각할 경우 void로 받는다.(insert에서 사용)
+
+![](https://images.velog.io/images/jinheesung/post/8f91cf6c-6304-454c-84af-c5ff80f6ffbd/image.png)
+
+selectbox 들어가는 값을 설정하려면 id, name을 모두 통일 시켜준다.
